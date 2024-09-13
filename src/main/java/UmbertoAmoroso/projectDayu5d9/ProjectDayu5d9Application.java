35815e07a27e122f8a51d902563290b2478997b2
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-09-13 11:54:18 +0200

    Initial commit

---
+++ src/main/java/UmbertoAmoroso/projectDayu5d9/ProjectDayu5d9Application.java
@@ -0,0 +1,13 @@
+package UmbertoAmoroso.projectDayu5d9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDayu5d9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDayu5d9Application.class, args);
+	}
+
+}
---
+++ src/test/java/UmbertoAmoroso/projectDayu5d9/ProjectDayu5d9ApplicationTests.java
@@ -0,0 +1,13 @@
+package UmbertoAmoroso.projectDayu5d9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDayu5d9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
